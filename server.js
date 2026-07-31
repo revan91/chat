7fe594cfe7cfd103e663d8fd3180058cee762fa5
revan91 <abcd9118172@gmail.com>
2026-07-31 22:45:04 +0800

    2

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+// 斷線偵測
+const io = new Server(server, {
+    pingTimeout: 60000,  // 伺服器每隔 60 秒會向手機/瀏覽器發送一次 ping 包。
+    pingInterval: 60000, // 當伺服器發出 ping 後，如果超過 60 秒沒有收到前端的 pong 回應，伺服器就會判定該用戶已斷線。
+});
 
 app.use(express.static(path.join(__dirname)));
 
